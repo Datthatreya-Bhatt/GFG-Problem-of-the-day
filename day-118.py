72765a0a2e27f9fe4810ea0d7b7c78b74fff17b8
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2024-04-03 08:08:23 +0530

    day 118

---
+++ day-118.py
@@ -0,0 +1,19 @@
+#   https://www.geeksforgeeks.org/problems/kth-common-ancestor-in-bst/1
+
+
+
+
+class Solution:
+    def kthCommonAncestor(self, root, k, x, y):
+        # we have to make sure x < y
+        if x > y:
+            x, y = y, x
+        ancestors, node = [root.data], root
+        while not (x <= node.data <= y):
+            if x < node.data:
+                node = node.left
+            else:
+                node = node.right
+            ancestors.append(node.data)
+        return ancestors[-k] if len(ancestors) >= k else -1
+        # Code here
