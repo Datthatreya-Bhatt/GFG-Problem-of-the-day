812287755f594e7b7983f12a8f1cc0beb331129d
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2024-04-30 07:36:34 +0530

    day 145

---
+++ day-145.py
@@ -0,0 +1,27 @@
+#       https://www.geeksforgeeks.org/problems/add-two-numbers-represented-by-linked-lists/1
+
+
+
+class Solution:
+    #Function to add two numbers represented by linked list.
+    def addTwoLists(self, num1, num2):
+        summ1=""
+        summ2=""
+        while num1:
+            summ1=summ1+str(num1.data)
+            num1=num1.next
+        while num2:
+            summ2=summ2+str(num2.data)
+            num2=num2.next
+        temp=int(summ1)+int(summ2)
+        summ1=str(temp)
+        head=None
+        for i in range(len(summ1)-1,-1,-1):
+            temp=Node(int(summ1[i]))
+            temp.next=head
+            head=temp
+        return head
+
+
+        # code here
+        # return head of sum list
