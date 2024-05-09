a86221bfb855e08e04db99a15218ad684c444659
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2024-05-09 07:53:19 +0530

    day 154

---
+++ day-154.js
@@ -0,0 +1,12 @@
+//      https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1
+
+
+
+
+class Solution {
+    // Code here
+    divisorGame(n) {
+        // your code here
+        return n%2 === 0;
+    }
+}
