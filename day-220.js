a6c61f626bcc26730f458240e47558ea60b75c70
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2024-07-14 09:20:25 +0530

    day 220

---
+++ day-220.js
@@ -0,0 +1,12 @@
+//      https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
+
+
+class Solution {
+
+    segregate0and1(arr) {
+
+        // code here
+        arr.sort((a, b)=> a-b);
+        
+    }
+}
