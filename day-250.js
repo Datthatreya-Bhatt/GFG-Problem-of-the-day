34b35a29e6eda20c1696d32d2652f570bd7fd06c
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2024-08-13 08:03:57 +0530

    day 250

---
+++ day-250.js
@@ -0,0 +1,12 @@
+//      https://www.geeksforgeeks.org/problems/square-root/1
+
+
+
+
+class Solution {
+
+    floorSqrt(n) {
+        // your code here
+        return (Math.floor(Math.sqrt(n)));
+    }
+}
